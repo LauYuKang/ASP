2c787da1df8ebe53da51cf3d023c5cc732b1d526
carlospker12 <172714L@mymail.nyp.edu.sg>
2019-08-05 10:36:30 +0800

    commit

--- eadLab5/2FA.aspx.cs
+++ eadLab5/2FA.aspx.cs
@@ -12,10 +12,58 @@ namespace eadLab5
 {
     public partial class _2FA : System.Web.UI.Page
     {
-        
+        string MYDBConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+
+        string af = null;
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            string userid = Session["adminNo"].ToString();
+            string af = get2FA(userid);
+            if (af == "1")
+            {
+                ButtonEnable2FA.Visible = false;
+            }
+            else if( af =="0")
+            {
+                ButtonDisable2FA.Visible = false;
+            }
+            else
+            {
+
+            }
+        }
+
+        protected string get2FA(string userid)
+        {
+            string s = null;
+            SqlConnection connection = new SqlConnection(MYDBConnectionString);
+            string sql = "select FA FROM [Student] WHERE AdminNo=@USERID";
+            SqlCommand command = new SqlCommand(sql, connection);
+            command.Parameters.AddWithValue("@USERID", userid);
+            try
+            {
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        if (reader["FA"] != null)
+                        {
+                            if (reader["FA"] != DBNull.Value)
+                            {
+                                s = reader["FA"].ToString();
+                                af = s;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.ToString());
+            }
+            finally { connection.Close(); }
+            return s;
         }
 
         protected void ButtonEnable2FA_Click(object sender, EventArgs e)
